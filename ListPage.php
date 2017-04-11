4e0ee50cda7c23bea6a50c6daf43b88507cd9326
lukebickell <luke@bickell.com>
2017-04-11 19:35:39 -0400

    simplify list page, add lat and long to event page

--- ListPage.php
+++ ListPage.php
@@ -255,7 +255,8 @@
   <?php
 
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["search"])) {
-
+	include("include.php");
+	
 	// This class implements an output method so that oracle_query can be called
     class outputObject {
       function output($statement){
@@ -281,7 +282,7 @@
               }
               echo "(No summary available)";
             }
-            if(isset($row->WEAPON_TYPE_TXT)){
+            /*if(isset($row->WEAPON_TYPE_TXT)){
               echo "<p><b>Weapon: </b>".$row->WEAPON_TYPE_TXT.": ".$row->WEAPON_SUBTYPE_TXT;
             }
             if(isset($row->NHOSTKID)){
@@ -303,7 +304,7 @@
 				} else {
 					echo "<p><b>Property Damage: </b> Unknown";
 				}
-			}
+			}*/
 			
 			// THIS LINE OPENS THE EVENT_ID SPECIFIC PAGE
 			echo "<p><a href='#;' class='button' onclick=\"window.open('getEvent.php?q=" . $row->EVENT_ID . "')\">See more info...</a></p>";
@@ -315,10 +316,10 @@
         }
       }
     }
-    include("include.php");
-    $spec = new outputObject();
-
-    oracle_query($query, $spec);
+	$spec = new outputObject();
+	//$query = "SELECT * FROM 
+	oracle_query($query, $spec);
+	echo $query;
   }
   ?>
 
